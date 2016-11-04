e695b1f2e378aa2da81d89923ebe9c59bc942604
Jack Murphy <jmurphy.web@gmail.com>
2016-11-04 14:14:59 +0000

    fix small linting errors that i was using for testing #3

--- example/server.js
+++ example/server.js
@@ -1,7 +1,8 @@
 'use strict';
+
 var Hapi = require('hapi');
 var server = new Hapi.Server();
-var Hoek = require('hoek')
+var Hoek = require('hoek');
 var tags = require('../');
 
 server.connection({ port: process.env.PORT || 3000 });
