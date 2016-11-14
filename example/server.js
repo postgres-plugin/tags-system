8cef46c01ee4fb56ca686e80303e04a30c56fcbd
SimonLab <simon.labondance@gmail.com>
2016-11-14 23:46:16 +0000

    refactor structure plugin

--- example/server.js
+++ example/server.js
@@ -1,77 +1,47 @@
 'use strict';
 
 var Hapi = require('hapi');
-var Hoek = require('hoek');
 var fs = require('fs');
 
-var server = new Hapi.Server();
 var tagsData = JSON.parse(fs.readFileSync('./example/tags.json').toString());
 var tags = require('../lib/index.js');
 var pg = require('pg');
 
-
 function init (port, pgConfig, callback) {
+  var server = new Hapi.Server();
   var tagsPool = new pg.Pool(pgConfig);
 
   server.connection({ port: port });
-
-  server.route([{
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      return reply('hello');
-    }
-  }]);
 
   server.register([{
     register: tags,
     options: {
       tags: tagsData,
       tagsPool: tagsPool,
-      databaseName: pgConfig.databaseName
+      databaseName: pgConfig.database
     }
   }], function (err) {
-    Hoek.assert(!err, err);
+    if (err) {
+      return callback(err);
+    }
+    server.route([{
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+        var allTags = request.getTags(function (error, listTags) {
+          console.log(error);
+          console.log('############');
+          console.log(listTags);
+          return reply(listTags);
+        });
 
-    return callback(null, server, tagsPool);
+      }
+    }]);
+
+    return server.start(function (errorStart) {
+      return callback(errorStart, server, tagsPool);
+    });
   });
-
-  //
-  // server.register([{
-  //   register: tags,
-  //   options: {
-  //     tags: tagsData,
-  //     tagsPool: tagsPool,
-  //     databaseName: pgConfig.database
-  //   }
-  // }], function (err) {
-  //   console.log('AAAAAAAAAA');
-  //   Hoek.assert(!err, err);
-  //   // if (err) {
-  //   //   return callback(err);
-  //   // }
-  //
-  //   server.route([{
-  //     method: 'GET',
-  //     path: '/home',
-  //     handler: function (request, reply) {
-  //       // tagsPool.connect(function (connErr, client, done) {
-  //         // Hoek.assert(!connErr, connErr);
-  //         // done();
-  //
-  //         return reply('home');
-  //         // client.query('select * from testtags', function (dberr, res) {
-  //         //   Hoek.assert(!dberr, dberr);
-  //         //   done();
-  //         //
-  //         //   return reply(res.rows);
-  //         // });
-  //       // });
-  //     }
-  //   }]);
-  //
-  //   return callback(null, server, tagsPool);
-  // });
 }
 
 module.exports = init;
--- example/start.js
+++ example/start.js
@@ -2,20 +2,9 @@
 
 var Hoek = require('hoek');
 var init = require('./server.js');
+var config = require('../config/load-config.js');
 
-var config = {
-  user: 'postgres',
-  database: 'tags',
-  password: '',
-  host: 'localhost',
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000
-};
-
-init(process.env.PORT || 3000, config, function (err, server) { // eslint-disable-line
+init(process.env.PORT || 3000, config, function (err, server) {
   Hoek.assert(!err, err);
-
-  server.start();
   process.stdout.write('server listening on port ' + server.info.uri + '\n');
 });
