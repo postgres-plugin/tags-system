14761029f279b1b4039a738015535f695ae703e9
Jack Murphy <jmurphy.web@gmail.com>
2016-11-17 22:38:40 +0000

    fix start.js arguments and only one travis db

--- example/start.js
+++ example/start.js
@@ -4,7 +4,7 @@ var Hoek = require('hoek');
 var init = require('./server.js');
 var config = require('../config/load-config.js');
 
-init(process.env.PORT || 3000, config, function (err, server) {
+init(config, function (err, server) {
   Hoek.assert(!err, err);
   process.stdout.write('server listening on port ' + server.info.uri + '\n');
 
