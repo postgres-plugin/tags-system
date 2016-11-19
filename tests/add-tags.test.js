3b6584d278fdd294d3341d026a4ac602b40f7a3e
Jack Murphy <jmurphy.web@gmail.com>
2016-11-19 18:23:58 +0000

    debug travis

--- tests/add-tags.test.js
+++ tests/add-tags.test.js
@@ -5,20 +5,24 @@ var addTags = require('../lib/queries/add-tags.js');
 var init = require('../example/server.js');
 var config = require('../config/load-config.js');
 var createChallenges = require('./helpers/create-challenges.js');
+var tagsData = require('../example/tags.json');
+var categoriesData = require('../example/categories.json');
 
 test('addTags function formats a valid query string', function (t) {
   var actual = addTags('challenges', 0, [0, 3, 9]);
   var expectedQuery = 'INSERT INTO tags_challenges (tags_id, challenges_id) '
-   + 'VALUES (0, 0),(3, 0),(9, 0);';
+  + 'VALUES (0, 0),(3, 0),(9, 0);';
 
   t.equal(actual, expectedQuery, 'The query for addTags is formatted');
   t.end();
 });
 
 test('Create tags_challenges and add tags', function (t) {
+  config.tagsData = tagsData;
+  config.categoriesData = categoriesData;
   init(config, function (err, server, pool) {
     if (err) {
-      return t.fail();
+      return t.fail(err);
     }
 
     return createChallenges(pool, function (error) {
